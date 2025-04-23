ab0bf9049bcd2e7243a21047c90da268446a184b
kain3x6 <dolbaebakysok@gmail.com>
2025-04-23 21:50:00 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,25 +2,26 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
+import os
 
 def pytest_addoption(parser):
     parser.addoption("--browser_name", action="store", default="chrome")
 
 @pytest.fixture(scope="function")
 def browser_init(request):
-    browser_name = request.config.getoption("browser_name") or "chrome"
+    browser_name = request.config.getoption("browser_name")
 
     if browser_name == "chrome":
         options = webdriver.ChromeOptions()
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-    
+        options.binary_location = "/usr/bin/google-chrome"  # Путь к установленному Chrome
+
         driver = webdriver.Chrome(
-            service = ChromeService(ChromeDriverManager().install()),
-            options = options
+            service=ChromeService(executable_path="/usr/bin/chromedriver"),
+            options=options
         )
 
     elif browser_name == "firefox":
